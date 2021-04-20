a94039315ec2d371a7fbc724a5a9c4878e3e0f9e
Sheilla Chairiandita <66354489+shch03@users.noreply.github.com>
2021-04-21 00:11:51 +0800

    About

---
+++ about.blade.php
@@ -0,0 +1,10 @@
+@extends ('master')
+
+@section('title', 'About Us')
+
+@section('content')
+    <h1> ABOUT US </h1>
+    <p> The use of the application to manage and organize your book list.</p>
+    <br>
+    <p> Assignment <br> Name: Sheilla Chairiandita <br>Matric No.: 1810502 <br> Section: 2 </p>
+@endsection
